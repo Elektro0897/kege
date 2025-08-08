e514102b92e1a1182e2c3a51dcb194fa949f352c
Elektro <elektro0309@mail.ru>
2025-08-08 19:32:27 +0200

    РАБота с библиотеками

---
+++ Python.Base/ln_5.py
@@ -0,0 +1,22 @@
+# работа с библиотеками
+
+# подключение библиотеки
+# import random
+
+# подключение библиотеки через псевдоним
+# import random as rm
+
+# подключение одной команды из библиотеки
+# from random import randint
+
+# подключение нескольких команд из библиотеки
+# from random import randint, choice
+
+# подключение всех команд из библиотеки
+# from random import *
+
+# подключение одной команды из библиотеки через псевдоним
+# from random import randint as rn
+
+# подключение нескольких команд из библиотеки через псевдонимы
+# from random import randint as rn, choice as ch
