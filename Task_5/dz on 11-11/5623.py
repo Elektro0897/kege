d60a21d14a11361b6ad064c155b387aa8207ce19
Elektro0897 <elektro0309@mail.ru>
2025-11-11 16:36:37 +0200

    дз на 11.11

---
+++ Task_5/dz on 11-11/5623.py
@@ -0,0 +1,10 @@
+for n in range(1, 100000):
+    r = bin(n)[2:]
+    if r.count('1') % 2 == 0:
+        r = '1' + r[:2] + '10'
+    else:
+        r = '10' + r[2:] + '1'
+    r = int(r, 2)
+    if r >= 202:
+        print(n)
+        break
