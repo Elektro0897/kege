37fb6afd95993bfa264592063e33be52e0e9eddc
Elektro <elektro0309@mail.ru>
2025-11-25 19:59:53 +0200

    14 задания

--- dz on 25-11/9918.py
+++ dz on 25-11/9918.py
@@ -1,2 +1,8 @@
 from string import printable as alf
+for x in alf[:]:
+    for y in alf[:]:
+        num1 = int(f'73{x}1{y}', 67)
+        num2 = int(f'49{y}6', x)
         num = num1 + num2
+        if num:
+            print(num)
