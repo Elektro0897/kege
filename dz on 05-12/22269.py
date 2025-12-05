1cbe30f0fac28d12f5e7666b8a1d4c8834bbc375
Elektro0897 <elektro0309@mail.ru>
2025-12-05 20:05:32 +0200

    дз на 05.12.

--- dz on 05-12/22269.py
+++ dz on 05-12/22269.py
@@ -1,23 +1,22 @@
-from string import printable as alf
 def conv(num, sys):
     res = ''
     while num:
-        res += alf[num % sys]
+        res += str(num % sys)
         num //= sys
     return res[::-1]
 ans = []
 for n in range(1, 100000):
     r = conv(n, 5)
-    if int(r) % 10 == 0:
-        r.replace('1', '*')
-        r.replace('4', '1')
-        r.replace('*', '4')
-        r = '33' + r
+    if r[-1] == '0':
+        r = '33' + r.replace('1', '*').replace('4', '1').replace('*', '4')
     else:
-        r = r +'42'
-        r = r[:-1] + '3'
+        r = '3' + r[1:] + '42'
     r = int(r, 5)
-    if r < 1922:
+    if r == 497:
         ans.append([r, n])
-print(max(ans))
-print('неправильный ответ: 74')
+max_r = max(ans)[0]
+all_n = []
+for r, n in ans:
+    if r == max_r:
+        all_n.append(n)
+print(min(all_n))
--- dz on 05-12/8472.py
+++ dz on 05-12/8472.py
@@ -1,14 +1,19 @@
-from string import printable as alf
-def convert(num, sys):
-    res = ''
-    while num:
-        res += alf[num % sys] # выдаёт тут ошибку
-        num //= sys
-    return res[::-1]
-for x in range(100):
-    num1 = convert(f'7a{x}0123', 100) # выдаёт тут ошибку
-    num2 = convert(f'1ba64{x}', 100) # выдаёт тут ошибку
-    num3 = convert(f'{x}98012c', 100) # выдаёт тут ошибку
-    num4 = num1 - num2 + num3
-    if num4 % 21 == 0:
-        print(convert(num4 // 21, 6))
+# from string import printable as alf
+# def convert(num, sys):
+#     res = ''
+#     while num:
+#         res += alf[num % sys] # выдаёт тут ошибку
+#         num //= sys
+#     return res[::-1]
+# for x in range(100):
+#     num1 = convert(f'7a{x}0123', 100) # выдаёт тут ошибку
+#     num2 = convert(f'1ba64{x}', 100) # выдаёт тут ошибку
+#     num3 = convert(f'{x}98012c', 100) # выдаёт тут ошибку
+#     num4 = num1 - num2 + num3
+#     if num4 % 21 == 0:
+#         print(convert(num4 // 21, 6))
+
+for x in range(1, 100):
+    num1 = 7*100**5 + 10*100**4 x*100**3 + 0*100**2 + 1*100**1 + 2*100 + 3
+    num2 = ...
+    num3 = ...
