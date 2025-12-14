30d4b9026aa6beaee46807c6b943749c95c4a1dc
Elektro0897 <elektro0309@mail.ru>
2025-12-14 16:55:56 +0200

    дз на 18.11.

---
+++ dz on 18-11/4867.py
@@ -0,0 +1,10 @@
+# sum1 = 0
+# sum2 = 0
+# for n in range(2, 10):
+#     if str(n)[0] != 0:
+#         for i in str(n):
+#             if int(i) % 2 == 0:
+#                 sum1 += int(i)
+#             if n[::2]:
+#                 sum2 += int(i)
+# print(abs(sum1-sum2))
