46f8b0f721e9ff3080df00b81354820c33018436
Elektro0897 <elektro0309@mail.ru>
2025-12-14 16:55:47 +0200

    дз на 28.11.

--- dz on 28-11/20181.py
+++ dz on 28-11/20181.py
@@ -4,9 +4,6 @@ for n in range(1, 100000):
         r = r + bin(bin(n).count('1'))[2:]
     else:
         r = '1' + r + '101'
-<<<<<<< HEAD
-=======
     if int(r, 2) > 350:
->>>>>>> 6f1200c (тест на 28.11. и чут чут задач)
         print(n)
         break
