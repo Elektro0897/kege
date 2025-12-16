c24b80b2f3bdfd51e477070a431755ba797309bd
Elektro <elektro0309@mail.ru>
2025-12-16 19:56:23 +0200

    дз на 16.12.

--- B dz on 16-12/8665.py
+++ B dz on 16-12/8665.py
@@ -3,12 +3,11 @@ from string import *
 k = 0
 for val in product(printable[:12], repeat=7):
     val = ''.join(val)
-    ans = ''.join(val)
-    for i in val:
-        if int(i, 12) % 2 == 0:
-            ans.replace(i, '+')
-        else:
-            ans.replace(i, '*')
-    if val.count('b') == 2 and ('*+*+*+*' in ans or '+*+*+*+' in ans):
+    if val[0] != 0 and val.count('b') == 2:
+        for i in '02468a':
+            val = val.replace(i, '+')
+        for i in '13579b':
+            val = val.replace(i, '*')
+    if '**' not in val and '++' not in val:
         k += 1
 print(k)
