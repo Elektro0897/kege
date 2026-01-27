c551523e24564d7374fcb247ed38206b5e4433e4
Elektro0897 <elektro0309@mail.ru>
2026-01-27 19:54:14 +0200

    задание 5

--- A task_5/Theory.py
+++ A task_5/Theory.py
@@ -23,7 +23,7 @@
 #     while num != 0:
 #         res += str(num % sys)
 #         num //= sys
-#     return res[::-1]
+#     return res[::-1] if res else '0'
 # print(convert(25, 3))
 
 #ZZZZZZZZZZZZZZZZZZZZZOOOOOOOOOOOOOOOOOOOOOOOOOOOVVVVVVVVVVVVVVVVVVVVVVVVVV
