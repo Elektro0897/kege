9883eb679c92ae30ab3dcbae1adedd758d9a4e6b
Elektro0897 <elektro0309@mail.ru>
2026-02-13 19:48:57 +0200

    теория 11-х заданий

---
+++ A task_11/theory.py
@@ -0,0 +1,30 @@
+# 1855
+# from math import *
+# L = 101
+# N = 10 + 4090
+# i = ceil(log2(N)) # - биты
+# I = ceil(L * i / 8) # - байты
+# print(I * 2048 // 2 ** 10)
+# ответ: 330
+
+# 23270
+# from math import *
+# for L in range(1, 10**9):
+#     N = 10 + 27
+#     i = ceil(log2(N))
+#     I = ceil(L * i / 8)
+#     if I * 3548 > 12 * 2**10:
+#         print(L)
+#         break
+# ответ: 5
+
+# 23195
+# from math import *
+# for N in range(1, 10**9):
+#     L = 172
+#     i = ceil(log2(N))
+#     I = ceil(L * i / 8)
+#     if I * 356984 >= 54 * 2**20:
+#         print(N)
+#         break
+# 129
