a2ec190a5cd994ae2fbe411eee1d58fb86530192
Elektro0897 <elektro0309@mail.ru>
2026-02-16 18:39:35 +0200

    дз на 17.02.

---
+++ C E dz on 17-02/5736.py
@@ -0,0 +1,17 @@
+def f(num):
+    d = set()
+    for i in range(2, int(num**.5) + 1):
+        if num % i == 0:
+            d |= {i, num // i}
+    if len(d) > 1:
+        if max(d) % 7 == 0:
+            return max(d)
+    return 0
+k = 0
+for n in range(10**9, 10**20):
+    if str(n) == str(n)[::-1]:
+        if m := f(n):
+            print(n, m)
+            k += 1
+            if k == 5:
+                break
