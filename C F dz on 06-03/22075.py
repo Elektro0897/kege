dc0e4a70ae69016b40bf85a846c5d529efe44af6
Elektro <elektro0309@mail.ru>
2026-03-06 18:12:02 +0200

    дз на 06.03.

--- C F dz on 06-03/22075.py
+++ C F dz on 06-03/22075.py
@@ -1,6 +1,5 @@
 def dig(x, y):
-    if str(x)[0] == str(y)[0]: return True
-    return False
+    return str(x)[0] == str(y)[0]
 def f(x):
     return ((not dig(x, 28)) and dig(x, 47)) <= (x > a - 20)
 ans = []
