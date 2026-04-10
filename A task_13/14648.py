f70e2b21b9d378c1036386d9aaa135ff40a04cd0
Elektro0897 <elektro0309@mail.ru>
2026-04-10 19:40:44 +0200

    13-е задания

--- A task_13/14648.py
+++ A task_13/14648.py
@@ -3,6 +3,6 @@ ip = ip_address('218.48.192.56')
 k = 0
 for mask in range(22, 16, -1):
     net = ip_network(f'218.48.192.0/{mask}', False)
-    if ip in net.hosts():
+    if ip in net:
         k += 1
 print(k)
