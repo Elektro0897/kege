616a98e856331f43ba5bda3b2c6a13009b436276
Elektro0897 <elektro0309@mail.ru>
2026-04-11 15:36:35 +0200

    26-е задания

---
+++ A task_26/17687.py
@@ -0,0 +1,11 @@
+with open(r'./files/26_17687.txt') as file:
+    n = int(file.readline())
+    prices = [int(i) for i in file]
+
+prices = sorted(prices, reverse=True)
+
+k = 9
+luck_price_customer = sum(prices) - sum(prices[:n // k])
+real_price_customer = sum(prices) - sum(prices[k - 1::k])
+
+print(luck_price_customer, real_price_customer)
