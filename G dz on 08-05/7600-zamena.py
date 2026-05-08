97f0075c5ad4fd2ae5c4794f2f53285c06dcf957
Elektro0897 <elektro0309@mail.ru>
2026-05-08 18:14:05 +0200

    дз на 08.05.

--- G dz on 08-05/7600-zamena.py
+++ G dz on 08-05/7600-zamena.py
@@ -2,6 +2,7 @@ with open(r'./files/24_7600.txt') as file:
     data = file.readline()
 for i in 'QSR':
     data = data.replace(i, '*')
-data = data.replace('**', ' ')
+data = data.replace('**', '* *')
+data = data.replace('**', '* *')
 data = data.split()
 print(len(max(data, key=len)))
