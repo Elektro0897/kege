25dab5b399c02bcc305166b4a04b9ae76fc1b7ad
Elektro0897 <elektro0309@mail.ru>
2026-06-15 17:56:08 +0200

    24-е задания

---
+++ A task_24/23206-re.py
@@ -0,0 +1,8 @@
+from re import *
+with open(r'./files/24_23206.txt') as file:
+    data = file.readline()
+
+pattern = r'[02468][^02468S]*([S][^02468S]*){35}'
+
+matches = [match.group() for match in finditer(pattern, data)]
+print(len(max(matches, key=len)))
